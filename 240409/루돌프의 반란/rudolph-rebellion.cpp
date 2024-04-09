d935af245ff3c750cb0d1c662f15a07e5f7e90fa
서지원 <ccxz84@naver.com>
2024-04-09 18:28:49 +0900

    Title: 루돌프의 반란, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240409/루돌프의 반란/rudolph-rebellion.cpp
+++ 240409/루돌프의 반란/rudolph-rebellion.cpp
@@ -1,212 +1,223 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <climits>
+#include <cmath>
 
 using namespace std;
 
-vector<int> solution(int n, int m, int p, int c, int d, vector<struct pos>& santa, struct pos rudol);
-
-int dirR[4] = {1, 0, -1, 0};
-int dirC[4] = {0, 1, 0, -1};
-
-#define ru -2
-#define none -1
-
 struct pos {
-    int r;
-    int c;
+    int x;
+    int y;
 };
 
-int main() {
-    int n,m,p,c,d;
-    vector<struct pos> santa;
-    struct pos rudol;
-    vector<int> score;
+vector<struct pos> santa;
+vector<vector<int>> board;
+struct pos rudol;
+vector<bool> out;
+vector<int> stun;
+vector<int> score;
 
-    cin>>n>>m>>p>>c>>d;
+int dirX[4] = {1, 0, -1, 0};
+int dirY[4] = {0, 1, 0, -1};
 
-    santa = vector<struct pos>(p);
+int n, m, p, c, d;
 
-    cin>>rudol.r>>rudol.c;
-    rudol.r -= 1;
-    rudol.c -= 1;
-    for(int i = 0; i < p; ++i) {
-        int index, row, column;
-        cin>>index>>row>>column;
-        santa[index-1].r = row - 1;
-        santa[index-1].c = column - 1;
+void print_rudol() {
+    cout<<"rudol pos: "<<rudol.x<<' '<<rudol.y<<'\n';
+}
+
+void print_santa() {
+    for (int i = 0; i < p; ++i) {
+        cout<<"santa "<<i<<" pos: "<<santa[i].x<<' '<<santa[i].y<<" score: "<<score[i]<<" stun: "<<stun[i]<<" out: "<<out[i]<<'\n';
+    }
+}
+
+// 루돌프 기준 가장 가까운 산타 찾기
+int find_santa() {
+    int dis = INT_MAX;
+    int index = -1;
+
+    pair<int, pair<int, int>> current = {INT_MAX, {0, 0}};
+
+    for (int i = 0; i < p; ++i) {
+        if(out[i]) continue;
+        int diffX = santa[i].x - rudol.x;
+        int diffY = santa[i].y - rudol.y;
+        pair<int, pair<int, int>> diff = {diffX * diffX + diffY * diffY, {-diffX, diffY}};
+
+        if (diff < current) {
+            current = diff;
+            index = i;
+        }
     }
 
-    score = solution(n, m, p, c, d, santa, rudol);
+    return index;
+}
+
+bool is_range(struct pos pos) {
+    return pos.x >= 0 && pos.x < n && pos.y >= 0 && pos.y < n;
+}
+
+void chain(struct pos pos, struct pos dir) {
+    struct pos target = pos;
+    int next = board[target.x][target.y];
+    while (next >= 0) {
+        int index = board[target.x][target.y];
+        int newR = target.x + dir.x, newC = target.y + dir.y;
+
+        board[target.x][target.y] = -2;
+
+        if (is_range({newR, newC})) {
+            santa[index] = {newR, newC};
+            next = board[newR][newC];
+            board[newR][newC] = index;
+        } else {
+            out[index] = true;
+            next = -1;
+        }
+    }
+}
+
+// 루돌프 이동
+void move_rudol(int index, int turn) {
+    struct pos target = santa[index];
+    int diffX = target.x - rudol.x;
+    int diffY = target.y - rudol.y;
+    struct pos dir = { diffX == 0 ? 0 : diffX / abs(diffX), diffY == 0 ? 0 : diffY / abs(diffY) };
+
+    board[rudol.x][rudol.y] = -2;
+    rudol = { rudol.x + dir.x, rudol.y + dir.y };
+
+    if (board[rudol.x][rudol.y] == index) {
+        int newR = target.x + dir.x * c , newC = target.y + dir.y * c; 
+        score[index] += c;
+        board[target.x][target.y] = -2;
+
+        if (is_range({newR, newC})) {
+            santa[index] = {newR, newC};
+
+            // 연쇄 작용 체크
+            chain({newR, newC}, dir); // 옮겨온 위치 파라미터
+            board[newR][newC] = index;
+            stun[index] = turn + 1;
+        } else {
+            out[index] = true;
+        }
+    }
+    
+    board[rudol.x][rudol.y] = -1;
+}
+
+// 산타 이동
+void move_santa(int turn) {
+    for (int i = 0; i < p; ++i) {
+        if (stun[i] >= turn || out[i]) continue;
+
+        int diffX = santa[i].x - rudol.x;
+        int diffY = santa[i].y - rudol.y;
+        int dis = diffX * diffX + diffY * diffY;
+        struct pos dir = {0, 0};
+
+        for (int j = 0; j < 4; ++j) {
+            struct pos new_pos = {santa[i].x + dirX[j], santa[i].y + dirY[j]};
+
+            if (!(is_range(new_pos) && board[new_pos.x][new_pos.y] < 0)) continue;
+
+            diffX = new_pos.x - rudol.x;
+            diffY = new_pos.y - rudol.y;
+
+            int diff = diffX * diffX + diffY * diffY;
+
+            if (diff < dis) {
+                dir = {dirX[j], dirY[j]};
+                dis = diff;
+            }
+        }
+
+        // cout<<"santa dir: "<<dir.x<<' '<<dir.y<<'\n';
+
+        board[santa[i].x][santa[i].y] = -2;
+        santa[i] = { santa[i].x + dir.x, santa[i].y + dir.y };
+
+        if (board[santa[i].x][santa[i].y] == -1) {
+            int newR = santa[i].x - dir.x * d , newC = santa[i].y - dir.y * d;
+
+            score[i] += d;
+            
+            if(is_range({newR, newC})) {
+                santa[i] = {newR, newC};
+                // cout<<newR<<newC<<-dir.x<<-dir.y<<'\n';
+                // cout<<santa[1].x<<santa[1].y<<'\n';
+                chain({newR, newC}, {-dir.x, -dir.y}); // 옮겨온 위치 파라미터
+                board[newR][newC] = i;
+                stun[i] = turn + 1;
+            } else {
+                out[i] = true;
+            }
+        } else {
+            board[santa[i].x][santa[i].y] = i;
+        }
+    }
 
     for(int i = 0; i < p; ++i) {
+        if (!out[i]) ++score[i];
+    }
+}
+
+void solution() {
+
+    // print_rudol();
+    // print_santa();
+
+    for(int z = 0; z < m; ++z) {
+        // 산타 찾기
+        int index = find_santa();
+
+        // cout<<"rudol move\n";
+        // 루돌프 이동
+        move_rudol(index, z);
+
+        // print_rudol();
+        // print_santa();
+
+        // cout<<"santa move\n";
+        // 산타 이동
+        move_santa(z);
+
+        // print_rudol();
+        // print_santa();
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); 
+
+    cin>>n>>m>>p>>c>>d;
+    cin>>rudol.x>>rudol.y;
+
+    rudol.x -= 1;
+    rudol.y -= 1;
+
+    santa = vector<struct pos>(p, {0, 0});
+    out = vector<bool>(p, false);
+    board = vector<vector<int>>(n, vector<int>(n, -2));
+    stun = vector<int>(p, -1);
+    score = vector<int>(p, 0);
+
+    for(int i = 0; i < p; ++i) {
+        int index;
+        int x, y;
+        cin>>index>>x>>y;
+
+        santa[index-1] = {x-1, y-1};
+    }
+
+    solution();
+
+    for (int i = 0; i < p; ++i) {
         cout<<score[i]<<' ';
     }
     cout<<'\n';
     return 0;
-}
-
-int find_santa(vector<struct pos>& santa, struct pos rudol, vector<bool>& out) {
-    int index = -1;
-    int dis = INT_MAX;
-
-    for (int i = 0; i < santa.size(); ++i) {
-        if (out[i]) continue;
-
-        bool qe = false;
-        int diff = (santa[i].r - rudol.r) * (santa[i].r - rudol.r) 
-        + (santa[i].c - rudol.c) * (santa[i].c - rudol.c);
-
-        if (diff < dis) {
-            qe = true;
-        } else if (diff == dis) {
-            if (santa[index].r < santa[i].r) {
-                qe = true;
-            } else if (santa[index].r == santa[i].r && santa[index].c < santa[i].c) {
-                qe = true;
-            }
-        }
-
-        if (qe) {
-            dis = diff;
-            index = i;
-        }
-    }
-    return index;
-}
-
-bool isOutOfBounds(int r, int c, int n) {
-    return r >= n || r < 0 || c >= n || c < 0;
-}
-
-void check_knockback(vector<struct pos>& santa, vector<vector<int>>& board, vector<bool>& out, struct pos dir, int index, int n) {
-    int next = board[santa[index].r][santa[index].c];
-    while(next > 0) {
-        int i = next;
-        int newR = santa[i].r - dir.r;
-        int newC = santa[i].c - dir.c;
-
-        if (!isOutOfBounds(newR, newC, n)) {
-            board[santa[i].r][santa[i].c] = none;
-            santa[i].r = newR;
-            santa[i].c = newC;
-            next = board[newR][newC];
-            board[newR][newC] = i;
-        } else {
-            board[santa[i].r][santa[i].c] = none;
-            out[i] = true;
-            next = 0;
-        }
-    }
-}
-
-vector<int> solution(int n, int m, int p, int c, int d, vector<struct pos>& santa, struct pos rudol) {
-    vector<vector<int>> board(n, vector<int>(n, none));
-    vector<int> stun(p, 0);
-    vector<bool> out(p, false);
-    vector<int> score(p, 0);
-
-    board[rudol.r][rudol.c] = ru;
-
-    for(int i = 0; i < p; ++i) {
-        board[santa[i].r][santa[i].c] = i;
-    }
-
-    for(int z = 0; z < m; ++z) {
-        int index = find_santa(santa, rudol, out);
-        struct pos dir;
-
-        if (index == -1) break;
-
-        dir.r = (santa[index].r == rudol.r) ? 0 : ((santa[index].r - rudol.r) / abs(santa[index].r - rudol.r));
-        dir.c = (santa[index].c == rudol.c) ? 0 : ((santa[index].c - rudol.c) / abs(santa[index].c - rudol.c));
-
-        if (board[rudol.r + dir.r][rudol.c + dir.c] > none) {
-            // 새로운 위치를 계산
-            int newR = santa[index].r + dir.r * c;
-            int newC = santa[index].c + dir.c * c;
-
-            // 경계를 벗어나는지 체크
-            if (isOutOfBounds(newR, newC, n)) {
-                board[santa[index].r][santa[index].c] = none;
-                out[index] = true;
-            } else {
-                // 위치 업데이트
-                board[santa[index].r][santa[index].c] = none;
-                santa[index].r = newR;
-                santa[index].c = newC;
-                check_knockback(santa, board, out, dir, index, n); // 이 함수의 세부 구현은 여기서 생략
-                board[newR][newC] = index;
-            }
-            score[index] += c;
-            stun[index] += 2;
-        } 
-
-        board[rudol.r][rudol.c] = none;
-        rudol.r += dir.r;
-        rudol.c += dir.c;
-        board[rudol.r][rudol.c] = ru;
-
-        for (int i = 0; i < p; ++i) {
-            if (out[i]) continue;
-            if (stun[i] > 0) {
-                --stun[i];
-                ++score[i];
-                continue;
-            }
-
-            struct pos dir;
-            int diff = (santa[i].r - rudol.r) * (santa[i].r - rudol.r) + (santa[i].c - rudol.c) * (santa[i].c - rudol.c);
-
-            for(int j = 0; j < 4; ++j) {
-                int newR = santa[i].r + dirR[j];
-                int newC = santa[i].c + dirC[j];
-
-                if (isOutOfBounds(newR, newC, n) || board[newR][newC] > none) continue;
-
-                int temp = (newR - rudol.r) * (newR - rudol.r) + (newC - rudol.c) * (newC - rudol.c);
-
-                if (temp < diff) {
-                    dir.r = dirR[j];
-                    dir.c = dirC[j];
-                    diff = temp;
-                }
-            }
-
-            if (board[santa[i].r + dir.r][santa[i].c + dir.c] == ru) {
-                int newR = santa[i].r - dir.r * (d - 1);
-                int newC = santa[i].c - dir.c * (d - 1);
-
-                if (isOutOfBounds(newR, newC, n)) {
-                    board[santa[i].r][santa[i].c] = none;
-                    out[i] = true;
-                    score[i] -= 1;
-
-                } else {
-                    // 위치 업데이트
-                    board[santa[i].r][santa[i].c] = none;
-                    santa[i].r = newR;
-                    santa[i].c = newC;
-                    check_knockback(santa, board, out, dir, i, n);
-                    board[newR][newC] = i;
-                }
-                score[i] += d + 1;
-                stun[i] += 1;
-            } else {
-                int newR = santa[i].r + dir.r;
-                int newC = santa[i].c + dir.c;
-
-                board[santa[i].r][santa[i].c] = none;
-                santa[i].r = newR;
-                santa[i].c = newC;
-                board[newR][newC] = i;
-                ++score[i];
-            }
-        }
-
-        
-    }
-
-    return score;
 }
