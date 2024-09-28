2b2d8eac17c624aceae19d90549b0246657ed791
서지원 <ccxz84@naver.com>
2024-09-29 01:43:48 +0900

    Title: 코드트리 메신저, Time: 354ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/코드트리 메신저/codetree-messenger.cpp
+++ 240929/코드트리 메신저/codetree-messenger.cpp
@@ -1,307 +1,158 @@
 #include <iostream>
-#include <unordered_map>
-#include <fstream>
 #include <vector>
-#include <queue>
-#include <unordered_set>
+
 using namespace std;
 
-ifstream inputFile("./input.txt");
-istream* input = &cin;
-ofstream outputFile("./output.txt");
-ofstream debugFile("./debug.txt");
-ostream* output = &cout;
+#define MAX_N 100001
+#define MAX_D 22
 
-// 메신저 준비
-// N은 노드 갯수 1 ~ N
-// 각 노드는 부모를 하나씩 가지고 있음
-// 권한 메시지 보내면 c만큼 위로 올라가서 알람 전해짐
-// 0은 메인이므로 권한, 알람 설정 영향 안받음
+int n, q;
+int a[MAX_N], p[MAX_N], val[MAX_N];
+bool noti[MAX_N];
+int nx[MAX_N][MAX_D];
 
-// 알림망 설정
-// 알림망 설정을 토글 하는 기능
-// OFF 면 해당 노드를 지나가는 알람을 전송하지 않음
-
-// 권한 세기 변경
-// 특정 노드의 권한 값을 변경
-
-// c1번 채팅방과 c2번 채팅방으로 부모를 서로 변경함
-// c1, c2는 같은 depth에 있음.
-
-// 알림 받을 수 있는 채팅방 조회
-// 해당 노드가 몇개의 자식으로 부터 알림을 받을 수 있는지 체크
-
-int n;
-
-struct node {
-    int pid, power;
-    bool on = true;
-    unordered_set<int> availableSet,cids;
-    int depth = 0;
-};
-
-vector<node> tree;
-
-unordered_set<int> dfs(int id, int depth) {
-    tree[id].depth = depth;
-    auto& cur = tree[id];
-    unordered_set<int> cids;
-
-    for (auto child : cur.cids) {
-        unordered_set<int> childs = dfs(child, depth + 1);
-
-        for (auto index : childs) {
-            cids.insert(index);
-        }
+// 초기 설정 값을 받아옵니다.
+void init() {
+    // 부모 채팅과 채팅의 권한 정보를 입력받습니다.
+    for(int i = 1; i <= n; i++) {
+        cin >> p[i];
     }
-
-    for (auto index : cids) {
-        auto& child = tree[index];
-        if (child.depth - cur.depth <= child.power) {
-            cur.availableSet.insert(index);
-        }
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+        // 채팅의 권한이 20을 초과하는 경우 20으로 제한합니다.
+        if(a[i] > 20) a[i] = 20;
     }
-    cids.insert(id);
-
-    return cids;
-}
-
-void initMessanger() {
-    for (int i = 0; i < n; ++i) {
-        *input >> tree[i + 1].pid;
-        tree[tree[i + 1].pid].cids.insert(i + 1);
-    }
-
-    for (int i = 0; i < n; ++i) {
-        *input >> tree[i + 1].power;
-    }
-       dfs(0, 0);
-}
-
-void toggleParent(int id, bool vaule) {
-    queue<int> parents;
-
-    parents.push(tree[id].pid);
-
-    while (!parents.empty()) {
-        int pid = parents.front();
-        parents.pop();
-
-        if (pid == -1) break;
-
-        if (!tree[id].on) {
-            for (auto index : tree[id].availableSet) {
-                tree[pid].availableSet.erase(index);
-            }
-            tree[pid].availableSet.erase(id);
-        }
-        else if(tree[id].on) {
-            for (auto index : tree[id].availableSet) {
-                auto& child = tree[index];
-                if (child.depth - tree[pid].depth <= child.power) {
-                    tree[pid].availableSet.insert(index);
-                }
-            }
-            if (tree[id].depth - tree[pid].depth <= tree[id].power) {
-                tree[pid].availableSet.insert(id);
-            }
-        }
-
-        if (!tree[pid].on) break;
-
-        parents.push(tree[pid].pid);
-    }
-}
-
-void toggleSwitch() {
-    int id;
-
-    *input >> id;
-
-    tree[id].on = !tree[id].on;
-
-    toggleParent(id, tree[id].on);
-}
-
-void changePowerParent(int id) {
-    queue<int> parents;
-
-    if (!tree[id].on) return;
-
-    parents.push(tree[id].pid);
-
-    while (!parents.empty()) {
-        int pid = parents.front();
-        parents.pop();
-
-        if (pid == -1) break;
-
-        if (tree[id].depth - tree[pid].depth <= tree[id].power) {
-            tree[pid].availableSet.insert(id);
-        }
-        else {
-            tree[pid].availableSet.erase(id);
-        }
-
-        if (!tree[pid].on) break;
-
-        parents.push(tree[pid].pid);
-    }
-}
-
-void changePower() {
-    int id, power;
-
-    *input >> id >> power;
-
-    tree[id].power = power;
-
-    changePowerParent(id);
-}
-
-void swapParent(int c1, int c2) {
-    queue<int> parents;
-
-    parents.push(tree[c1].pid);
-
-    unordered_set<int> dataSet;
-
-    while (!parents.empty()) {
-        int pid = parents.front();
-        parents.pop();
-
-        if (pid == -1) break;
-
-        dataSet.insert(pid);
-
-        for (auto index : tree[c1].availableSet) {
-            tree[pid].availableSet.erase(index);
-        }
-
-        tree[pid].availableSet.erase(c1);
-
-        if (tree[c2].on) {
-            for (auto index : tree[c2].availableSet) {
-                auto& child = tree[index];
-                if (child.depth - tree[pid].depth <= child.power) {
-                    tree[pid].availableSet.insert(index);
-                }
-            }
-            if (tree[c2].depth - tree[pid].depth <= tree[c2].power) {
-                tree[pid].availableSet.insert(c2);
-            }
-        }
-
-        if (!tree[pid].on) break;
-
-        parents.push(tree[pid].pid);
-    }
-
-    parents.push(tree[c2].pid);
-
-    while (!parents.empty()) {
-        int pid = parents.front();
-        parents.pop();
-
-        if (pid == -1) break;
-
-        if (dataSet.count(pid) <= 0) {
-            for (auto index : tree[c2].availableSet) {
-                tree[pid].availableSet.erase(index);
-            }
-
-            tree[pid].availableSet.erase(c2);
-        }
-
-        if (tree[c1].on) {
-            for (auto index : tree[c1].availableSet) {
-                auto& child = tree[index];
-                if (child.depth - tree[pid].depth <= child.power) {
-                    tree[pid].availableSet.insert(index);
-                }
-            }
-            if (tree[c1].depth - tree[pid].depth <= tree[c1].power) {
-                tree[pid].availableSet.insert(c1);
-            }
-        }       
-
-        if (!tree[pid].on) break;
-
-        parents.push(tree[pid].pid);
-    }
-}
-
-int runcount;
-
-void swap() {
-    int c1, c2;
     
-    *input >> c1 >> c2;
-
-    swapParent(c1, c2);
-
-    int c1Pid = tree[c1].pid;
-    int c2Pid = tree[c2].pid;
-    
-    tree[c1Pid].cids.erase(c1);
-    tree[c1Pid].cids.insert(c2);
-
-    
-    tree[c2Pid].cids.erase(c2);
-    tree[c2Pid].cids.insert(c1);
-
-    tree[c2].pid = c1Pid;
-    tree[c1].pid = c2Pid;
-
+    // nx 배열과 val 값을 초기화합니다.
+    for(int i = 1; i <= n; i++) {
+        int cur = i;
+        int x = a[i];
+        nx[cur][x]++;
+        // 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while(p[cur] && x) {
+            cur = p[cur];
+            x--;
+            if(x) nx[cur][x]++;
+            val[cur]++;
+        }
+    }
 }
 
-void printChatroom() {
-    int id;
+// 채팅의 알림 상태를 토글합니다.
+void toggle_noti(int chat) {
+    if(noti[chat]) {
+        int cur = p[chat];
+        int num = 1;
+        // 상위 채팅으로 이동하며 noti 값에 따라 nx와 val 값을 갱신합니다.
+        while(cur) {
+            for(int i = num; i <= 21; i++) {
+                val[cur] += nx[chat][i];
+                if(i > num) nx[cur][i - num] += nx[chat][i];
+            }
+            if(noti[cur]) break;
+            cur = p[cur];
+            num++;
+        }
+        noti[chat] = false;
+    } else {
+        int cur = p[chat];
+        int num = 1;
+        // 상위 채팅으로 이동하며 noti 값에 따라 nx와 val 값을 갱신합니다.
+        while(cur) {
+            for(int i = num; i <= 21; i++) {
+                val[cur] -= nx[chat][i];
+                if(i > num) nx[cur][i - num] -= nx[chat][i];
+            }
+            if(noti[cur]) break;
+            cur = p[cur];
+            num++;
+        }
+        noti[chat] = true;
+    }
+}
 
-    *input >> id;
+// 채팅의 권한의 크기를 변경합니다.
+void change_power(int chat, int power) {
+    int bef_power = a[chat];
+    power = min(power, 20);  // 권한의 크기를 20으로 제한합니다.
+    a[chat] = power;
 
-    *output << tree[id].availableSet.size() << '\n';
+    nx[chat][bef_power]--;
+    if(!noti[chat]) {
+        int cur = p[chat];
+        int num = 1;
+        // 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while(cur) {
+            if(bef_power >= num) val[cur]--;
+            if(bef_power > num) nx[cur][bef_power - num]--;
+            if(noti[cur]) break;
+            cur = p[cur];
+            num++;
+        }
+    }
+
+    nx[chat][power]++;
+    if(!noti[chat]) {
+        int cur = p[chat];
+        int num = 1;
+        // 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while(cur) {
+            if(power >= num) val[cur]++;
+            if(power > num) nx[cur][power - num]++;
+            if(noti[cur]) break;
+            cur = p[cur];
+            num++;
+        }
+    }
+}
+
+// 두 채팅의 부모를 교체합니다.
+void change_parent(int chat1, int chat2) {
+    bool bef_noti1 = noti[chat1];
+    bool bef_noti2 = noti[chat2];
+
+    if(!noti[chat1]) toggle_noti(chat1);
+    if(!noti[chat2]) toggle_noti(chat2);
+
+    swap(p[chat1], p[chat2]);
+
+    if(!bef_noti1) toggle_noti(chat1);
+    if(!bef_noti2) toggle_noti(chat2);
+}
+
+// 해당 채팅의 val 값을 출력합니다.
+void print_count(int chat) {
+    cout << val[chat] << "\n";
 }
 
 int main() {
-    int q;
-
-    if (inputFile.is_open()) {
-        input = &inputFile;  // 파일을 열었다면 파일로 입력을 받음
-    }
-
-    if (outputFile.is_open()) {
-        output = &outputFile;  // 파일을 열었다면 출력도 파일로
-    }
-
-    *input >> n >> q;
-    tree = vector<node>(n+1);
-    tree[0] = { -1, 0, true };
-
-    for (int i = 0; i < q; ++i) {
-        int code;
-        *input >> code;
-        switch (code) {
-        case 100:
-            //cout << "100\n";
-            initMessanger();
-            break;
-        case 200:
-            //cout << "200\n";
-            toggleSwitch();
-            break;
-        case 300:
-            //cout << "300\n";
-            changePower();
-            break;
-        case 400:
-            swap();
-            break;
-        case 500:
-            printChatroom();
-            break;
+    cin >> n >> q;
+    while(q--) {
+        int query;
+        cin >> query;
+        if(query == 100) {
+            init();
+        }
+        else if(query == 200) {
+            int chat;
+            cin >> chat;
+            toggle_noti(chat);
+        }
+        else if(query == 300) {
+            int chat, power;
+            cin >> chat >> power;
+            change_power(chat, power);
+        }
+        else if(query == 400) {
+            int chat1, chat2;
+            cin >> chat1 >> chat2;
+            change_parent(chat1, chat2);
+        }
+        else if(query == 500) {
+            int chat;
+            cin >> chat;
+            print_count(chat);
         }
     }
-    
     return 0;
 }
